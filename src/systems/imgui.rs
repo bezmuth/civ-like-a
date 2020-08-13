78e9d2e22b5fd90f18301d7257fd541799d66cfb
Bensmuth <benkel97@protonmail.com>
2020-08-14 00:59:35 +0100

    made imgui usable as debug menu

--- src/systems/imgui.rs
+++ src/systems/imgui.rs
@@ -1,22 +1,50 @@
-extern crate amethyst;
-extern crate amethyst_imgui;
+use crate::game::{CurrentPlayer};
 use amethyst::{
-	input::{InputBundle, StringBindings},
-	prelude::*,
-	renderer::{bundle::RenderingBundle, types::DefaultBackend, RenderToWindow},
-	utils::application_root_dir,
+    core::{
+        transform::Transform,
+        Time
+    },
+    derive::SystemDesc,
+    renderer::{Camera},
+    ecs::prelude::{Join, Read, System, SystemData, ReadExpect},
+    input::{InputHandler, StringBindings}, 
 };
 
-use amethyst_imgui::RenderImgui;
+use amethyst_imgui::imgui::*;
 
-#[derive(Default, Clone, Copy)]
-pub struct Imgui;
-impl<'s> amethyst::ecs::System<'s> for Imgui {
-	type SystemData = ();
 
-	fn run(&mut self, _: Self::SystemData) {
-		amethyst_imgui::with(|ui| {
-			ui.show_demo_window(&mut true);
-		});
-	}
+#[derive(SystemDesc)]
+pub struct Imgui{
+    pub toggled : bool,
+}
+
+impl<'s> System<'s> for Imgui {
+    type SystemData = (
+        Read<'s, InputHandler<StringBindings>>,
+        ReadExpect<'s, CurrentPlayer>,
+    );
+
+
+    fn run(&mut self, (input, currentplayer): Self::SystemData) {
+        //TODO: fix debug toggle
+        if input.action_is_down("debug").unwrap(){
+            self.toggled = !self.toggled
+        }
+        if self.toggled{
+            amethyst_imgui::with(|ui| {
+                Window::new(im_str!("Debug"))
+                .size([300.0, 110.0], Condition::FirstUseEver)
+                .build(ui, || {
+                    ui.text(format!("Current Player Number: {}", currentplayer.playernum));
+                    ui.bullet_text(im_str!("cool"));
+                    ui.separator();
+                    let mouse_pos = ui.io().mouse_pos;
+                    ui.text(format!(
+                        "Mouse Position: ({:.1},{:.1})",
+                        mouse_pos[0], mouse_pos[1]
+                    ));
+                });
+            });
+        }
+    }
 }
