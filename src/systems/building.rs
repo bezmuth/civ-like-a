97acfea8414f2cf50e4380bd12e77a4117618ec9
Bensmuth <benkel97@protonmail.com>
2020-08-14 19:32:39 +0100

    Added the bottom panel

--- src/systems/building.rs
+++ src/systems/building.rs
@@ -1,23 +1,61 @@
 use crate::game::{Build, BuildingType};
 use amethyst::{
-    derive::SystemDesc,
-    ecs::prelude::{Read, System, SystemData, WriteExpect},
+    ecs::{Entities, prelude::{Read, System, WriteExpect, Write,WriteStorage}, Entity},
     input::{InputHandler, StringBindings}, 
+    ui::{UiFinder, UiEvent, UiEventType, UiButton, UiText}, 
+    shrev::{ReaderId, EventChannel}, 
+    shred::World,
+
 };
 
-#[derive(SystemDesc)]
-pub struct BuildSystem;
+
+#[derive(Default)] // allows ::default() to be called, forcing setup to be run
+pub struct BuildSystem{
+    event_reader: Option<ReaderId<UiEvent>>
+}
 
 impl<'s> System<'s> for BuildSystem {
     type SystemData = (
         Read<'s, InputHandler<StringBindings>>,
         WriteExpect<'s, Build>,
+        Entities<'s>,
+        Write<'s, EventChannel<UiEvent>>,
+        UiFinder<'s>,
         //WriteStorage<'s, TileEnts>
     );
 
-    fn run(&mut self, (input, mut build): Self::SystemData) {
-        if input.action_is_down("build").unwrap(){
-            build.mode = BuildingType::FaithBuilding;
+    fn run(&mut self, (input, mut build, entities, events, ui_finder): Self::SystemData) {
+        let reader = self
+            .event_reader
+            .as_mut()
+            .expect("BuildSystem::setup was not called correctly, try adding component with ::defauly()");
+
+        for event in events.read(reader) {
+            match event.event_type{
+                UiEventType::Click => {
+                    let clicked = event.target.id();
+                    // TODO: implement full build menu with all building types
+                    let build_button = ui_finder.find("Build_button").expect("Either: build_menu.ron was not loaded or the id was wrong").id();
+                    let demolish_button = ui_finder.find("Demolish_button").expect("Either: build_menu.ron was not loaded or the id was wrong").id();
+                    
+                    // * tried to use match but it didnt work? Got variable not used warnings?
+                    if clicked == build_button{ 
+                        println!("Build!");
+                        build.mode = BuildingType::FaithBuilding;
+                    } else if clicked == demolish_button {
+                        println!("Demolish")
+                    }
+
+
+                },
+                _ => {}
+            }
         }
+
     }
+
+    fn setup(&mut self, world: &mut World) {
+        self.event_reader = Some(world.fetch_mut::<EventChannel<UiEvent>>().register_reader());
+    }
+    
 }
