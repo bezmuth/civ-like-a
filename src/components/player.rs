c753fb7972239694a7c9e179e84f1fba9d899a00
Bensmuth <benkel97@protonmail.com>
2020-08-27 19:37:53 +0100

    implemented turns

--- src/components/player.rs
+++ src/components/player.rs
@@ -4,13 +4,13 @@ use amethyst::ecs::prelude::{DenseVecStorage, Component};
 // * this means you forgot to use &mut players in the .join iterator!
 
 pub struct Player{
-    pub num: i8,
+    pub num: i32,
     pub wood: i32,
     pub metal: i32,
     pub faith: i32,
 }
 impl Player {
-    pub fn new(num: i8) -> Player {
+    pub fn new(num: i32) -> Player {
         Player {
             num,
             wood: 0,
@@ -21,4 +21,9 @@ impl Player {
 }
 impl Component for Player { // Component therefore use ReadStorage  an
     type Storage = DenseVecStorage<Self>;
+}
+
+pub struct PlayersInfo{
+    pub count: i32,
+    pub current_player_num : i32,
 }
