b5fbd3b109efaaaae344c9820dbea4f1fbffd678
Bensmuth <benkel97@protonmail.com>
2020-10-21 19:53:49 +0100

    now check if the menu is shown using .contains

--- src/systems/building.rs
+++ src/systems/building.rs
@@ -11,8 +11,7 @@ use amethyst::{
 #[derive(Default)] // allows ::default() to be called, forcing setup to be run
 pub struct BuildSystem{
     event_reader: Option<ReaderId<UiEvent>>,
-    build_toggle: bool,
-    last_mode: bool,
+    run: bool,
 }
 
 impl<'s> System<'s> for BuildSystem {
@@ -24,56 +23,56 @@ impl<'s> System<'s> for BuildSystem {
         //WriteStorage<'s, TileEnts>
     );
 
-    fn run(&mut self, (mut build, events, ui_finder, mut hiddenpropagates): Self::SystemData) {
+    fn run(&mut self, (mut build, events, ui_finder, mut hidden_propagates): Self::SystemData) {
         let reader = self
             .event_reader
             .as_mut()
             .expect("BuildSystem::setup was not called correctly, try adding component with ::default()");
+        if !self.run{
+            if let Some(build_menu) = ui_finder.find("build_menu"){
+                let _  = hidden_propagates.insert(build_menu, HiddenPropagate::new()).unwrap();
+                self.run = true;
+            }
+        } else {
+            if let Some(build_menu) = ui_finder.find("build_menu"){
 
-
-        for event in events.read(reader) {
-            match event.event_type{
-                UiEventType::Click => { // Todo: block sheet input if clicking on a ui element
-                    
-                    let clicked = event.target.id();
-                    
-                    // * tried to use match but it didnt work? Got variable not used warnings?
-                    if clicked == ui_finder.find("Build_button").unwrap().id(){ 
-                        self.build_toggle = !self.build_toggle;
-                    } else if clicked == ui_finder.find("Demolish_button").unwrap().id() {
-                        build.mode = Some(BuildingType::Demolish);
-                    } else if clicked == ui_finder.find("Center_button").unwrap().id() {
-                        build.mode = Some(BuildingType::Center)
-                    } else if clicked == ui_finder.find("Barrack_button").unwrap().id() {
-                        build.mode = Some(BuildingType::Barrack)
-                    } else if clicked == ui_finder.find("WoodBuilding_button").unwrap().id() {
-                        build.mode = Some(BuildingType::WoodBuilding)
-                    } else if clicked == ui_finder.find("MetalBuilding_button").unwrap().id() {
-                        build.mode = Some(BuildingType::MetalBuilding)
-                    } else if clicked == ui_finder.find("ScienceBuilding_button").unwrap().id() {
-                        build.mode = Some(BuildingType::ScienceBuilding)
+                for event in events.read(reader) {
+                    match event.event_type{
+                        UiEventType::Click => { // Todo: block sheet input if clicking on a ui element
+                            
+                            let clicked = event.target.id();
+                            
+                            // * tried to use match but it didnt work? Got variable not used warnings?
+                            if clicked == ui_finder.find("Build_button").unwrap().id(){ 
+                                if !hidden_propagates.contains(build_menu){ // todo: adapt this to a hidden_propagates.contains
+                                    let _  = hidden_propagates.insert(build_menu, HiddenPropagate::new()).unwrap();
+                                }else if hidden_propagates.contains(build_menu){
+                                    let _  = hidden_propagates.remove(build_menu).unwrap();
+                                }
+                            } else {
+                                if clicked == ui_finder.find("Demolish_button").unwrap().id() {
+                                    build.mode = Some(BuildingType::Demolish);
+                                } else if clicked == ui_finder.find("Center_button").unwrap().id() {
+                                    build.mode = Some(BuildingType::Center)
+                                } else if clicked == ui_finder.find("Barrack_button").unwrap().id() {
+                                    build.mode = Some(BuildingType::Barrack)
+                                } else if clicked == ui_finder.find("WoodBuilding_button").unwrap().id() {
+                                    build.mode = Some(BuildingType::WoodBuilding)
+                                } else if clicked == ui_finder.find("MetalBuilding_button").unwrap().id() {
+                                    build.mode = Some(BuildingType::MetalBuilding)
+                                } else if clicked == ui_finder.find("ScienceBuilding_button").unwrap().id() {
+                                    build.mode = Some(BuildingType::ScienceBuilding)
+                                }
+                                let _  = hidden_propagates.insert(build_menu, HiddenPropagate::new()).unwrap();
+                            }
+                            
+                        },
+                        _ => {}
                     }
-                    
-                },
-                _ => {}
-            }
+                }
+            }    
         }
 
-
-        if let Some(build_menu) = ui_finder.find("build_menu"){
-            if self.build_toggle{
-                if !self.last_mode{
-                    let _  = hiddenpropagates.insert(build_menu, HiddenPropagate::new()).unwrap();
-                    self.last_mode = true;
-                }
-            } 
-            else {
-                if self.last_mode{
-                    let _  = hiddenpropagates.remove(build_menu).unwrap();
-                    self.last_mode = false
-                }
-            }
-        }
 
 
     }
