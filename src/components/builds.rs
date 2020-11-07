ce3db747931a92729fa8c25c6a2f31a6aef462f3
bezmuth <benkel97@protonmail.com>
2020-11-07 12:59:47 +0000

    Added: A TilePos vector Moved Building type to tiles.rs and renamed it tile type

--- src/components/builds.rs
+++ src/components/builds.rs
@@ -1,23 +1,48 @@
 use amethyst::ecs::prelude::{DenseVecStorage, Component};
-#[derive(Copy, Clone, PartialEq)]
-pub enum BuildingType{ // resource as it has no component implmentation, use READ and READEXPECT
-    Center = 5,
-    Barrack = 6,
-    WoodBuilding = 7,
-    MetalBuilding = 8,
-    ScienceBuilding = 9,
-    Demolish = 99,
-}
+use super::{unit::Unit, tiles::TileType};
+// pub struct UnitStack{
+//     top: i8,
+//     arr: [Option<Unit>; 8],
+//     pub repeat: bool,
+// }
+
+// impl UnitStack{
+//     fn new() -> UnitStack{
+//         UnitStack{top: 0, arr: [None; 8], repeat: false}
+//     }
+
+//     fn pop(&mut self) -> Option<Unit>{
+//         if self.top == 0{
+//             return None
+//         } else {
+//             if self.repeat{
+//                 return self.arr[self.top as usize];
+//             } else {
+//                 self.top = self.top - 1;
+//                 return self.arr[(self.top+1) as usize];
+//             }
+//         }
+//     }
+
+//     fn push(&mut self, unit : Unit) {
+//         let unote = Some(unit);
+//         self.arr[self.top as usize] = Some(unit);
+//         self.top = self.top + 1;
+//     }
+// }
+
+
 
 pub struct Build{
-    pub mode: Option<BuildingType>,
+    pub mode: Option<TileType>,
 }
 
-pub struct Building{ 
-    pub buildingtype: BuildingType,
+pub struct Building{ // todo make these private?
+    pub TileType: TileType,
     pub playernum: i32,
-    pub x: i32,
-    pub y: i32,
+    pub out_x: i32,
+    pub out_y: i32,
+    // unit_stack: UnitStack, // todo: move into a seperate component
 }
 impl Component for Building { // Component therefore use ReadStorage  an
     type Storage = DenseVecStorage<Self>;
--- src/components/tiles.rs
+++ src/components/tiles.rs
@@ -1,10 +1,32 @@
 use amethyst::ecs::{NullStorage, prelude::{DenseVecStorage, Component}};
-use super::BuildingType;
+
+#[derive(PartialEq, Eq, Copy, Clone)] // Partial Eq and Eq for comparisons
+                                      // Copy and Clone are used when I want to give 2 tiles on different layers the same pos
+pub struct TilePos{
+    pub x : i32,
+    pub y: i32,
+}
+
+impl Component for TilePos{
+    type Storage = DenseVecStorage<Self>;
+}
+
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType{ // resource as it has no component implmentation, use READ and READEXPECT
+    Empty = 4,
+    Center = 5,
+    Barrack = 6,
+    WoodBuilding = 100, // not implemented
+    MetalBuilding = 101, // not implemented
+    ScienceBuilding = 102, // not implemented
+    Location = 7,
+    Demolish = 99,
+}
+
 pub struct Tiles{ 
     pub player: i32,
-    pub buildingtype: Option<BuildingType>,
-    pub x: i32,
-    pub y: i32,
+    pub TileType: Option<TileType>,
 }
 impl Component for Tiles { // Component therefore use ReadStorage  an
     type Storage = DenseVecStorage<Self>;
